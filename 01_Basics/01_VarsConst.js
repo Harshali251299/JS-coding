34a9d14819ad2c3b4f703199a022509bfe2e47f3
Harshali251299 <harshali.datir99@gmail.com>
2025-04-17 07:19:13 +0000

    Variables and constants in JS

---
+++ 01_Basics/01_VarsConst.js
@@ -0,0 +1,10 @@
+const accId = 65438;
+// accId = 9999938; not allowed
+
+var name = "Harshali"; //never use this....this will cause issues while dealing with scope
+let address = "Home12"; //always use let
+color = "Yellow"; //never use this too
+
+console.table([accId,name,address,color]);
+
+
